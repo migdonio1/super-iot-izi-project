b7b2c127ce3a93038477a038ed3e924726acd16d
Raul Migdonio Rodriguez Te <raulmigdonio@gmail.com>
2016-09-27 17:40:03 -0500

    Se agrego socketio y se movio de carpeta el proyecto de android

---
+++ movil/app/src/main/java/iot/arduinocontroller/wifiController.java
@@ -0,0 +1,13 @@
+package iot.arduinocontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class wifiController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wifi_controller);
+    }
+}
