e360181bac791eaddebffbda35b77d037d77e577
Raul Migdonio Rodriguez Te <raulmigdonio@gmail.com>
2016-09-28 15:21:27 -0500

    Se agrego la integracion de Socketio

--- movil/app/src/main/java/iot/arduinocontroller/wifiController.java
+++ movil/app/src/main/java/iot/arduinocontroller/wifiController.java
@@ -2,6 +2,14 @@ package iot.arduinocontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
 
 public class wifiController extends AppCompatActivity {
 
@@ -9,5 +17,17 @@ public class wifiController extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_controller);
+        mSocket.connect();
     }
+
+    private Socket mSocket;
+
+    {
+        try {
+            mSocket = IO.socket("http://localhost:3000");
+        } catch (URISyntaxException e) {
+            Log.e("SOCKET IO", e.toString());
+        }
+    }
+
 }
